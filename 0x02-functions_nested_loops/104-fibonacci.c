7f2742d2fe3f49a81f8b7655c6b8e35f17eb0f3a
Ndiser <mandisandiser@gmail.com>
2023-07-19 19:29:03 -0700

    corrections

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,6 @@ int main(void)
 	unsigned long int i;
 	unsigned long int bef = 1;
 	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
 	unsigned long int bef1;
 	unsigned long int bef2;
 	unsigned long int aft1;
@@ -30,7 +29,6 @@ int main(void)
 	bef2 = (bef % 1);
 	aft1 = (aft / 1);
 	aft2 = (aft % 1);
-	
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 / 1));
@@ -42,4 +40,4 @@ int main(void)
 	}
 	printf("\n");
 	return (0);
-}
+
