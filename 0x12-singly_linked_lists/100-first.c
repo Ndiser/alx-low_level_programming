93bfbb9f286835ce806f8090e3547adbbed90e56
Ndiser <mandisandiser@gmail.com>
2023-08-24 16:43:20 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void first(void) __attribute__ ((constructor));
  */
 void first(void)
 {
-	printf("you're best! and yet, you must allow,\n");
+	printf("You're best! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
